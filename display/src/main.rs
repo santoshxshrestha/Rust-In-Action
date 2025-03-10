e18e2ef7435695b9c61a108a314d4d657773d311
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-10 15:32:30 +0545

    small activity by the use of the wirte and the fmt:Display

--- display/src/main.rs
+++ display/src/main.rs
@@ -53,4 +53,24 @@ fn main() {
         println!("Display: {}", point);
         println!("Debug: {:?}", point);
     }
+
+    {
+        #[derive(Debug)]
+        struct Complex {
+            real: f64,
+            imag: f64,
+        }
+        impl fmt::Display for Complex {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "{} + {}i", self.real, self.imag)
+            }
+        }
+
+        let vals = Complex {
+            real: 3.3,
+            imag: 7.2,
+        };
+        println!("Display: {}", vals);
+        println!("Debug: {:#?}", vals);
+    }
 }
