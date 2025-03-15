2b5afd429af20634fbce825c4780c43626835fdc
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-15 18:26:50 +0545

    some more

--- casting/src/main.rs
+++ casting/src/main.rs
@@ -11,4 +11,22 @@ fn main() {
         println!(" 300.0 as u8 is : {}", 300.0_f32 as u8);
         println!("{char_val}");
     }
+
+    {
+        let nan = f32::NAN as u32;
+        println!("The value of nan is :{nan}");
+        //the maning of the nan is not a number and they are not equal
+        assert_eq!(f32::NAN, f32::NAN);
+    }
+    unsafe {
+        // 300.0 as u8 is 44
+        println!(" 300.0 as u8 is : {}", 300.0_f32.to_int_unchecked::<u8>());
+        // -100.0 as u8 is 156
+        println!(
+            "-100.0 as u8 is : {}",
+            (-100.0_f32).to_int_unchecked::<u8>()
+        );
+        // nan as u8 is 0
+        println!("   nan as u8 is : {}", f32::NAN.to_int_unchecked::<u8>());
+    }
 }
