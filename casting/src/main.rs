ae120a6b7c2c33a4731f4efa39c165b811c1633b
Santosh Shrestha <santoshsantoshsthastha@gmail.com>
2025-03-15 19:57:05 +0545

    some more

--- casting/src/main.rs
+++ casting/src/main.rs
@@ -13,10 +13,10 @@ fn main() {
     }
 
     {
-        let nan = f32::NAN as u32;
-        println!("The value of nan is :{nan}");
-        //the maning of the nan is not a number and they are not equal
-        assert_eq!(f32::NAN, f32::NAN);
+        // let nan = f32::NAN as u32;
+        // println!("The value of nan is :{nan}");
+        // //the maning of the nan is not a number and they are not equal
+        // assert_eq!(f32::NAN, f32::NAN);
     }
     unsafe {
         // 300.0 as u8 is 44
@@ -29,4 +29,15 @@ fn main() {
         // nan as u8 is 0
         println!("   nan as u8 is : {}", f32::NAN.to_int_unchecked::<u8>());
     }
+
+    let i = 0;
+    let j = 1;
+    let k = 1.0;
+    let x = 51f32;
+    let y = 21i32;
+    println!("size of i in bytes is: {}", std::mem::size_of_val(&i));
+    println!("size of j in bytes is: {}", std::mem::size_of_val(&j));
+    println!("size of k in bytes is: {}", std::mem::size_of_val(&k));
+    println!("size of x in bytes is: {}", std::mem::size_of_val(&x));
+    println!("size of y in bytes is: {}", std::mem::size_of_val(&y));
 }
