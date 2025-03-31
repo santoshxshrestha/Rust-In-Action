2cda9b40fd3c74b90d7522dcf3468bc4fa09b5e5
santosh shrestha <santoshxshrestha@gmail.com>
2025-04-01 01:47:58 +0545

    some more closure related thing

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,5 +1,20 @@
 #![allow(unused)]
 use std::{mem, vec};
+
+//so we can use this this to apply the 
+//closure just once and drop the val or transfer the ownership out
+//there in the function 
+fn apply<F>(f:F)where 
+F:FnOnce() {
+    f();
+}
+
+fn apply_to_4<F>(f:F) -> i32 where
+F:Fn(i32) -> i32 {
+    f(4)
+}
+
+
 fn main() {
     let a = 3;
     let b = 52;
@@ -33,5 +48,33 @@ fn main() {
         let something = vec![42, 523, 423, 52];
         let contains = move |needle| something.contains(needle);
         println!("It contain the val:42 {}", contains(&42));
+    }
+
+    {
+        let greeting = "hello anish";
+        let mut farewell = "goodbye anish".to_owned();
+
+        let diary = || {
+            println!("I said {}", greeting);
+
+            farewell.push_str("!!!");
+            println!("Then I screamed {}",farewell);
+            println!("Now I can sleep. zzz");
+
+            mem::drop(farewell);
+
+        };
+        apply(diary);
+    }
+
+    //lets apply the same thing but this time we will just apply more 
+    //not just once 
+    {
+        let double = |x| 2*x;
+        println!("4 is doubled : {}", apply_to_4(double));
+
+        let triple = |x| 3*x;
+        println!("4 is tripled : {}", apply_to_4(triple));
+
     }
 }
