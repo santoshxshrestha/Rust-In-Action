31b19d41b0405e824cef3833bac851d5e8e209b8
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-07 21:33:03 +0545

    some more addition

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -13,15 +13,23 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
+
+async fn santosh() -> impl Responder {
+    HttpResponse::Ok().body("Hey there this is santosh shrestha")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("The server is running...");
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/santosh", web::get().to(santosh))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
+
