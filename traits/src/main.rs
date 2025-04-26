d73a04a5a321f8527814d984ddf3cd823a717797
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-27 00:28:40 +0545

    the way to do things properly

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,60 @@
+#![allow(unused)]
+
+use std::str::Chars;
+struct Class {
+    name: &'static str,
+    present: bool,
+    game: &'static str
+}
+
+trait Student {
+    fn new(name:&'static str) -> Self;
+
+    fn name(&self) -> &'static str;
+    fn game(&self) -> &'static str;
+
+    fn intro(&self){
+        println!("Hello my name is {}. And I play {}",self.name(), self.game());
+    }
+}
+
+impl Class {
+    fn is_present(&self) -> bool {
+        self.present
+    }
+
+    //the is_present is for the following use it has no use in the 
+    //main function 
+    fn call_parents(&mut self) {
+        if self.is_present(){
+            println!("{} is already present...",self.name);
+        }else {
+            println!("Calling the parents of {}.",self.name);
+            //assuming that after calling the parents the students will be 
+            //there in the class
+            self.present = true;
+        }
+    }
+}
+
+impl Student for Class{
+    fn new(name: &'static str) -> Class {
+        Class { name, present: false, game: "football" } } 
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn game(&self) -> &'static str {
+        "football"
+    }
+
+} 
+
 fn main() {
-    println!("Hello, world!");
+    let mut students = Class::new("Santosh");
+    students.present = true;
+    students.call_parents();
+    students.intro();
+
 }
