1189863f56d642e0957705f647cbc4458b87e53b
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-29 17:55:32 +0545

    some cmp lol it didn't went like I was expecting

---
+++ std_cmp/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let max_val=      std::cmp::max(42, 45);
+    println!("The maxmium value is {}.",max_val);
+
+    let min_val = std::cmp::min(523, 52);
+    println!("The minimum value is {}.",min_val);
+
+}
+
