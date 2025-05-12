36d58253fcd027a5a1e0e3aaa880a96a53065e0d
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-12 15:48:08 +0545

    done with the things here

--- path/src/main.rs
+++ path/src/main.rs
@@ -1,3 +1,30 @@
+#![allow(unused)]
+use std::path::{self, Path};
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("/home/santosh/dev/Rust-In-Action");
+
+    let display = path.display();
+    println!("{display}");
+
+    println!();
+    //while joining the path there should not be '/'  directory included again
+    let mut new_path = path.join("path/").join("src/");
+    println!("after joining the path and src {}",new_path.display());
+
+    println!();
+    new_path.push("main.rs");
+    println!("after pushing the main.rs{}",new_path.display());
+
+    println!();
+    new_path.set_file_name("lib.rs");
+    println!("after setting the file name to the lib.rs{}",new_path.display());
+
+    println!();
+
+    match new_path.to_str() {
+        None => panic!("latest path is not a valid UTF-8 sequence"),
+        Some(s) => println!("latest path is {}",s),
+        
+    }
 }
