b6216241ff2fa2051db058bebbdfdef87b76c548
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-05-12 16:41:26 +0545

    Done with the writing in the file

--- file_io/src/main.rs
+++ file_io/src/main.rs
@@ -1,12 +1,22 @@
 #![allow(unused)]
 use std::fs::File;
 use std::io::prelude::*;
+use std::panic;
 use std::path::Path;
 
+static LOREM_IPSUM: &str =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
+
 fn main() {
+    {
     let path = Path::new("hello.txt");
     let display = path.display();
-    println!("The path is {display}");
 
 
     let mut file = match File::open(&path) {
@@ -23,4 +33,22 @@ fn main() {
     }
     //after the file goes out of scope the 
     //"hello.txt" file gets automatically closed
+    }
+
+    println!("----------------------------------------------------------------");
+
+    {
+        let path = Path::new("write.txt");
+        let display = path.display();
+
+        let mut file  = match File::create(&path){
+            Err(why) => panic!("countn't create {}: {}",display, why),
+            Ok(file)=> file,
+        };
+// as_bytes is used here becasue the write_all does not allows 'statis string
+        match file.write_all(LOREM_IPSUM.as_bytes()) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why),
+            Ok(_) => println!("successfully wrote to {}",display),
+        }
+    }
 }
