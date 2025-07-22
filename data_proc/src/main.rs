9c71ab6107b3d7f9509e9ac51925654c4e5ea07b
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-22 17:37:35 +0545

    more threading

--- data_proc/src/main.rs
+++ data_proc/src/main.rs
@@ -2,15 +2,14 @@ use std::thread;
 use std::time::Duration;
 use tokio;
 
-pub async fn thread_testing() {
+pub async fn thread_testing() -> thread::JoinHandle<()> {
     //here we are spawing a thread and doning the work in that thread so we use the closure
-    let handle = thread::spawn(|| {
-        for i in 1..100 {
+    thread::spawn(|| {
+        for i in 1..4 {
             println!("hi number {i} from the spawned thread");
             thread::sleep(Duration::from_secs(1));
         }
-    });
-    handle.join().unwrap();
+    })
 }
 
 #[tokio::main]
@@ -18,5 +17,10 @@ async fn main() {
     println!("This is the first content of the main function");
 
     //we are calling the thread_testing function which is async fn
-    thread_testing().await;
+    let handle = thread_testing().await;
+
+    if let Err(e) = handle.join() {
+        eprintln!("you got some error joining the handle{e:?}")
+    }
+    println!("this is the content after the thread_testing function is joined");
 }
