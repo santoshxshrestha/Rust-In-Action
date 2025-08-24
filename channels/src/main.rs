de7975456774192dcee324dc91f8b235006afa7a
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-25 01:42:48 +0545

    update

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -1,43 +1,35 @@
 #![allow(unused)]
-use std::sync::mpsc::{self, Sender, Receiver};
+use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 
-//setting the cound of the thread to 3
+//setting the count of the thread to 3
 static NTHREADS: i32 = 3;
- 
-
 
 fn main() {
-// In summary, SyncSender is a version of Sender that uses synchronization to ensure that the sender blocks if the receiver isn't ready to consume messages,
-// giving you fine control over communication flow between threads.
-    let (tx, rx): (Sender<i32>, Receiver<i32>)  = mpsc::channel();
+    // In summary, SyncSender is a version of Sender that uses synchronization to ensure that the sender blocks if the receiver isn't ready to consume messages,
+    // giving you fine control over communication flow between threads.
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
     let mut children = Vec::new();
 
     for id in 0..NTHREADS {
-
         let thread_tx = tx.clone();
 
-    let child = thread::spawn(move || {
+        let child = thread::spawn(move || {
+            //here we are sending to rx and if we get any errors then the program will panic
+            thread_tx.send(id).unwrap();
+            println!("thread {} finished", id);
+        });
 
-        //here we are sending to rx and if we get any errors then the program will panic
-        thread_tx.send(id).unwrap();
-        println!("thread {} finished", id);
-
-    });
-
-    children.push(child);
-
+        children.push(child);
     }
 
-
     let mut ids = Vec::with_capacity(NTHREADS as usize);
-    for _ in 0..NTHREADS{
+    for _ in 0..NTHREADS {
         ids.push(rx.recv());
     }
 
     for child in children {
         child.join().expect("oops! the child thread panicked");
     }
-    println!("{:?}",ids);
-
+    println!("{:?}", ids);
 }
