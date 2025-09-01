7f6d6efd4a415243f67edb5f1e3c573e5864e437
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-01 14:39:39 +0545

    test rpint

---
+++ print/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("this is single small line ");
+    println!(
+        "{} {} {} {}",
+        "this is another line",
+        "this is after space of new line out there",
+        "this is another line here",
+        "and this is the final line for here"
+    )
+}
--- trash_test/src/main.rs
+++ trash_test/src/main.rs
@@ -1,7 +1,17 @@
 #![allow(unused)]
+use std::any::{type_name, type_name_of_val};
 use std::path::Path;
 use trash::os_limited::purge_all;
 use trash::os_limited::{self, restore_all};
 
 fn main() {
+    let content_to_delete = trash::os_limited::list()
+        .unwrap()
+        .into_iter()
+        .next()
+        .unwrap()
+        .name;
+    let truthy = content_to_delete == std::ffi::OsStr::new("test.txt");
+    println!("the content is {:#?}", content_to_delete);
+    println!("the truthy is {}", truthy);
 }
