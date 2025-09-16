c3fe6b0b866a884f81a1bad2977a82c01e1d4562
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-16 17:09:21 +0545

    update: arc is necessary for the threat safty

--- askamaxx/src/main.rs
+++ askamaxx/src/main.rs
@@ -1,20 +1,24 @@
 use actix_web::App;
 use actix_web::Responder;
 use actix_web::get;
+use actix_web::web;
 use actix_web::{self, HttpResponse, HttpServer};
 use askama::Template;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Template)]
 #[template(path = "index.html")]
 pub struct IndexTemplate {
-    pub count: u32,
+    pub count: usize,
 }
-static mut VAL: IndexTemplate = IndexTemplate { count: 0 };
 
 #[get("/")]
-pub async fn hello() -> impl Responder {
-    let template = unsafe {static &mut VAL };
-    template.count += 1;
+pub async fn hello(count: web::Data<Arc<AtomicUsize>>) -> impl Responder {
+    let template = IndexTemplate {
+        count: count.clone().fetch_add(1, Ordering::SeqCst),
+    };
+
     HttpResponse::Ok()
         .content_type("text/html")
         .body(template.render().unwrap())
@@ -22,9 +26,15 @@ pub async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let count = Arc::new(AtomicUsize::new(0));
     println!("Starting server at http://localhost:8080");
-    HttpServer::new(move || App::new().service(hello))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let clone_count = count.clone();
+    HttpServer::new(move || {
+        App::new()
+            .service(hello)
+            .app_data(web::Data::new(clone_count.clone()))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
