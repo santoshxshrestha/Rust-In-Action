332e769017a85b4ae71e8025d60a17b50c94afb6
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-17 00:35:43 +0545

    axkamaaa

--- askamaxx/src/main.rs
+++ askamaxx/src/main.rs
@@ -1,6 +1,8 @@
+#![allow(unused)]
 use actix_web::App;
 use actix_web::Responder;
 use actix_web::get;
+use actix_web::post;
 use actix_web::web;
 use actix_web::{self, HttpResponse, HttpServer};
 use askama::Template;
@@ -9,16 +11,17 @@ use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Template)]
 #[template(path = "index.html")]
-pub struct IndexTemplate {
-    pub count: usize,
+pub struct IndexTemplate;
+
+#[get("/count")]
+pub async fn count(count: web::Data<Arc<AtomicUsize>>) -> impl Responder {
+    let current_count = count.fetch_add(1, Ordering::AcqRel);
+    HttpResponse::Ok().body(format!("count: {}", current_count))
 }
 
 #[get("/")]
-pub async fn hello(count: web::Data<Arc<AtomicUsize>>) -> impl Responder {
-    let template = IndexTemplate {
-        count: count.clone().fetch_add(1, Ordering::SeqCst),
-    };
-
+pub async fn hello() -> impl Responder {
+    let template = IndexTemplate;
     HttpResponse::Ok()
         .content_type("text/html")
         .body(template.render().unwrap())
@@ -26,12 +29,16 @@ pub async fn hello(count: web::Data<Arc<AtomicUsize>>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let count = Arc::new(AtomicUsize::new(0));
+    let num = Arc::new(AtomicUsize::new(0));
+    let nums = Arc::new(32);
     println!("Starting server at http://localhost:8080");
-    let clone_count = count.clone();
+    let clone_count = num.clone();
+    let cloen_of_nums = nums.clone();
     HttpServer::new(move || {
         App::new()
             .service(hello)
+            .service(count)
+            .app_data(web::Data::new(cloen_of_nums.clone()))
             .app_data(web::Data::new(clone_count.clone()))
     })
     .bind("127.0.0.1:8080")?
--- flake.nix
+++ flake.nix
@@ -1,27 +1,27 @@
 {
   description = "Flake for Rust";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        rustc
-        cargo
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          rustc
+          cargo
 
-        # clippy
-        rustfmt
-        openssl
-        # rust-analyzer
-        # gcc
-        # pkg-config
-      ];
-      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          # clippy
+          rustfmt
+          openssl
+          # rust-analyzer
+          # gcc
+          # pkg-config
+        ];
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
     };
-  };
 }
