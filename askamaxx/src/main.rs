e19f90637b6a0b0b80d3c44200adeeaef1aa269e
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-17 22:17:28 +0545

    finally got it the use of the form out there in the seraver

--- askamaxx/src/main.rs
+++ askamaxx/src/main.rs
@@ -4,9 +4,12 @@ use actix_web::Responder;
 use actix_web::get;
 use actix_web::post;
 use actix_web::web;
+use actix_web::web::Form;
 use actix_web::{self, HttpResponse, HttpServer};
 use askama::Template;
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Template)]
@@ -27,9 +30,22 @@ pub async fn hello() -> impl Responder {
         .body(template.render().unwrap())
 }
 
+#[derive(Clone, Deserialize)]
 pub struct ContactInfo {
+    #[serde(default)]
     name: String,
+
+    #[serde(default)]
     email: String,
+}
+
+impl ContactInfo {
+    pub fn new(name: &str, email: &str) -> Self {
+        ContactInfo {
+            name: name.to_string(),
+            email: email.to_string(),
+        }
+    }
 }
 
 #[derive(Template)]
@@ -39,36 +55,48 @@ pub struct ContactsTemplate {
 }
 
 #[get("/contacts")]
-pub async fn contacts() -> impl Responder {
+pub async fn contacts(details: web::Data<Arc<Mutex<Vec<ContactInfo>>>>) -> impl Responder {
+    let data = details.lock().unwrap();
     let template = ContactsTemplate {
-        contacts: vec![
-            ContactInfo {
-                name: "Alice".to_string(),
-                email: "fukcer@gmail.com".to_string(),
-            },
-            ContactInfo {
-                name: "Bob".to_string(),
-                email: "anotherfucker@gmail.com".to_string(),
-            },
-        ],
+        contacts: data.to_vec(),
     };
+
     HttpResponse::Ok()
         .content_type("text/html")
         .body(template.render().unwrap())
 }
 
+#[post("/contacts")]
+pub async fn add_contact(
+    details: web::Data<Arc<Mutex<Vec<ContactInfo>>>>,
+    form: Form<ContactInfo>,
+) -> impl Responder {
+    let mut details = details.lock().unwrap();
+    details.push(ContactInfo::new(&form.name, &form.email));
+    HttpResponse::SeeOther()
+        .append_header(("Location", "/contacts"))
+        .finish()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let num = Arc::new(AtomicUsize::new(0));
-    // let contact_details: Vec<ContactInfo> = Vec::new();
-    let nums = Arc::new(32);
+    let contact_details: Arc<Mutex<Vec<ContactInfo>>> = Arc::new(Mutex::new(vec![
+        ContactInfo::new("Alice", "alice@gmail.com"),
+        ContactInfo::new("Bob", "bobthe_builder@gmail.com"),
+        ContactInfo::new("kolam", "kolam@gmail.com"),
+    ]));
+
     println!("Starting server at http://localhost:8080");
     let clone_count = num.clone();
+    let clone_contacts = Arc::clone(&contact_details);
     HttpServer::new(move || {
         App::new()
             .service(hello)
             .service(count)
             .service(contacts)
+            .service(add_contact)
+            .app_data(web::Data::new(clone_contacts.clone()))
             .app_data(web::Data::new(clone_count.clone()))
     })
     .bind("127.0.0.1:8080")?
