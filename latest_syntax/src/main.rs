26dfa32b5952fe2260c93a0cc2af1c5d026e9a4e
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-06 21:12:10 +0545

    refactor: switch ordering_in_atomics example to AtomicU64 and update thread usage

---
+++ latest_syntax/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // for _ in 1..5 {
+    //     println!("hello santosh")
+    // }
+    (1..5).map(|_| println!("hello santosh"));
+}
--- ordering_in_atomics/src/main.rs
+++ ordering_in_atomics/src/main.rs
@@ -1,10 +1,41 @@
 #![allow(unused)]
 use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::thread;
-
 fn main() {
-    let data = Arc::new(AtomicBool::new(false));
-    let data_clone = Arc::clone(&data);
-    thread::spawn(move || println!("the data is {:?}", data_clone.)).join();
+    // {
+    //     let atomic = Arc::new(AtomicU64::new(0));
+    //     let clone_of_atomic = atomic.clone();
+    //     let joinhandle = thread::scope(|s| {
+    //         thread::spawn(move || {
+    //             clone_of_atomic.store(100, Ordering::Release);
+    //         })
+    //     });
+    //     if let Err(e) = joinhandle.join() {
+    //         eprintln!("you got errro here {e:?}");
+    //     }
+    //
+    //     let another_joinhandle = thread::scope(|s| {
+    //         thread::spawn(move || {
+    //             let val = atomic.store(121, Ordering::Relaxed);
+    //             let val_to_be_printed = atomic.load(Ordering::Relaxed);
+    //             println!("the value is {val_to_be_printed}");
+    //         })
+    //     });
+    //
+    //     if let Err(e) = another_joinhandle.join() {
+    //         eprintln!("you got errro here {e:?}");
+    //     }
+    // }
+    let count = Arc::new(AtomicU64::new(0));
+    let joinhandle = thread::scope(|s| {
+        let clone_of_arc = Arc::clone(&count);
+        s.spawn(|| clone_of_arc.fetch_add(1, Ordering::Relaxed));
+
+        s.spawn(|| clone_of_arc.fetch_add(2, Ordering::Relaxed));
+
+        s.spawn(|| {
+            println!("the count now is {:?}",);
+        })
+    });
 }
