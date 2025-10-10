a60b8ae523f375cfe0e13309b1d55d4a6a3423b5
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-10 17:47:45 +0545

    this error

--- error_is_this/src/main.rs
+++ error_is_this/src/main.rs
@@ -1,2 +1,42 @@
-use thiserror;
-fn main() {}
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Errors {
+    #[error("You have to put there some name babe")]
+    NoName,
+
+    #[error("Who the hell are you {0}")]
+    DontKnowYou(String),
+
+    #[error("what the fwak why are you here {0}")]
+    WhyYouHere(String),
+}
+
+fn greet(name: &str) -> Result<String, Errors> {
+    match name.trim() {
+        "Anish" => Err(Errors::DontKnowYou(name.to_string())),
+        "Kamal" => Err(Errors::WhyYouHere(name.to_string())),
+        "" => Err(Errors::NoName),
+        _ => Ok(format!("Hello there {}", name)),
+    }
+}
+
+fn main() {
+    let anish = "Anish";
+    match greet(&anish) {
+        Ok(message) => println!("message: {}", message),
+        Err(e) => eprintln!("message: {}", e),
+    }
+
+    let kamal = "Kamal";
+    match greet(&kamal) {
+        Ok(message) => println!("message: {}", message),
+        Err(e) => eprintln!("message: {}", e),
+    }
+
+    let hair = " ";
+    match greet(&hair) {
+        Ok(message) => println!("message: {}", message),
+        Err(e) => eprintln!("message: {}", e),
+    }
+}
