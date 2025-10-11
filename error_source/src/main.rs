0eabb388d07ea629fdd923bf4789c5492349d27e
santoshxshrestha <santoshxshrestha@gmail.com>
2025-10-12 00:00:09 +0545

    some more

--- error_source/src/main.rs
+++ error_source/src/main.rs
@@ -1,10 +1,46 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum MeroErrors {
-    Msg(String),
-    #[source]
-    Source(std::io::Error),
+pub enum AgeError {
+    #[error("your age is less then zero")]
+    LessThenZero(String),
+
+    #[error("you are just born")]
+    EqualToZero(String),
+
+    #[error("this is the custom error that you are getting")]
+    CustomError {
+        #[source]
+        source: Box<dyn std::error::Error + 'static>,
+        description: String,
+    },
+}
+
+pub struct Age {
+    age: i32,
+}
+pub struct Class{
+    main: String,
+    func: String,
+
+}
+
+
+fn check_errors(age: Age) -> Result<String, AgeError> {
+    if age.age < 0 {
+        return Err(AgeError::LessThenZero(
+            "your age is in negative".to_string(),
+        ));
+    } else if age.age == 0 {
+        return Err(AgeError::EqualToZero("welcome to the earth".to_string()));
+    } else if age.age > 0 {
+        return Ok(format!("you got the following error{}", age.age));
+    } else {
+        Err(AgeError::CustomError {
+            source
+            description: "the error you just got is called custom error".to_string(),
+        })
+    }
 }
 
 fn main() {}
