bd9a95b732d739490175789cbb80f58e899e4ef6
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-09 21:57:24 +0545

    some tests for generics

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,47 +1,12 @@
-#![allow(unused)]
-use std::clone;
-use std::{fmt, ptr::slice_from_raw_parts};
-
-struct Student {
-    name: String,
-    class: u8,
-    rollno: u8,
-    address: String,
+fn add<T>(a: T, b: T) -> T
+where
+    T: std::ops::Add<Output = T>,
+{
+    return a + b;
 }
-impl Student {
-    fn new(name: String, class: u8, rollno: u8, address: String) -> Student {
-        Student {
-            name: name,
-            class: class,
-            rollno: rollno,
-            address: address,
-        }
-    }
-}
-
-impl fmt::Display for Student {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Hello there my name is {} . I reed in class {}, and my roll number is {}. I live in {}",
-            self.name, self.class, self.rollno, self.address
-        )
-    }
-}
-impl clone::Clone for Student {
-    fn clone(&self) -> Self {
-        Self {
-            name: self.name.clone(),
-            class: self.class,
-            rollno: self.rollno,
-            address: self.address.clone(),
-        }
-    }
-}
-
 fn main() {
-    let jason = Student::new("Json".to_string(), 14, 89, "Butwal".to_string());
-    let hello = jason.clone();
-    println!("{}", jason);
-    println!("{}", hello);
+    let a = 3;
+    let b = 23;
+
+    println!(" the sum of  two number a and b is: {} ", add(a, b));
 }
