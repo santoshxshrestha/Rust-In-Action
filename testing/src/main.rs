dad1be7833069cf2c37291ee7a7cecded103d443
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-13 13:26:41 +0545

    some tests

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,12 +1,36 @@
+use std::{any::type_name_of_val, process::Output};
+
 fn add<T>(a: T, b: T) -> T
 where
     T: std::ops::Add<Output = T>,
 {
     return a + b;
 }
-fn main() {
-    let a = 3;
-    let b = 23;
 
-    println!(" the sum of  two number a and b is: {} ", add(a, b));
+pub trait MeroTrait {
+    fn use_this(&self) -> String;
 }
+
+impl MeroTrait for i32 {
+    fn use_this(&self) -> String {
+        return format!("i32: {}", self);
+    }
+}
+impl MeroTrait for String {
+    fn use_this(&self) -> String {
+        return format!("String: {}", self);
+    }
+}
+impl MeroTrait for i64 {
+    fn use_this(&self) -> String {
+        return format!("i64: {}", self);
+    }
+}
+
+fn do_some_thing<T>(content: T) -> String
+where
+    T: MeroTrait<Output = String>,
+{
+    return type_name_of_val(&content).to_string();
+}
+fn main() {}
